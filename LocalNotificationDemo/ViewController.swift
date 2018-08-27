88218db066d083fe17eeb67c071adfd061b7af34
lehuudungle <huudung95d@gmail.com>
2018-08-27 08:41:17 +0700

    first update

--- LocalNotificationDemo/ViewController.swift
+++ LocalNotificationDemo/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import  UserNotifications
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+    var messageSubtitle = "Staff meeting in 20 minutes"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            
+        }
+        UNUserNotificationCenter.current().delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func buttonPress(_ sender: Any) {
+        sendNotification()
     }
-
-
+    
+    func sendNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Meeting Remider"
+        content.subtitle = messageSubtitle
+        content.body = "Don't forget to bring coffe"
+        content.badge = 1
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let requestIdentifier = "demoNotification"
+        let request = UNNotificationRequest(identifier: requestIdentifier, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { (error) in
+            
+        }
+        
+        let repeatAction = UNNotificationAction(identifier:"repeat",
+                                                title:"Repeat",options:[])
+        
+        let changeAction = UNTextInputNotificationAction(identifier: "change",
+                                                         title: "Change Message", options: [])
+        
+        let category = UNNotificationCategory(identifier: "actionCategory",
+                                              actions: [repeatAction, changeAction],
+                                              intentIdentifiers: [], options: [])
+        
+        content.categoryIdentifier = "actionCategory"
+        
+        UNUserNotificationCenter.current().setNotificationCategories([category])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
+    
 }
 
